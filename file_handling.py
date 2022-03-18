b7d6688a37c615a797bcd2d9e74d845ce3d2f315
Visakh <visakhcu96@gmail.com>
2022-03-18 12:09:39 +0530

    added functionalities to go back and rename files

--- file_handling.py
+++ file_handling.py
@@ -1,17 +1,72 @@
 import os
+import stat
+import string
+
+
+def get_drives():
+    """Get all the drives and devices connected on this PC/laptop."""
+
+    drives = []
+    for letter in string.ascii_uppercase:
+        if os.path.exists(f"{letter}:"):
+            drives.append(f"{letter}:")
+    return drives
 
 
 def open(path):
     """Opens the file if the path points to a file or returns a list of the folders/files in that specific directory."""
+    # for displaying root directories
+    if not path:
+        return ROOT_DIRECTORIES
+
+    # opens the choice if it's a file
     if os.path.isfile(path):
         os.startfile(path)
         return None
 
-    return os.listdir(path)
+    # return list of files in the directory chosen
+    files = [file for file in os.listdir(path) if not is_hidden(file, path)]
+    return files
 
 
-def create_path(suffix, curr_dir):
-    """Creates the appropriate path for the chosen folder/file."""
+def create_path(curr_dir, suffix=None, back=False):
+    """Creates the appropriate path.
+
+    Parameters
+    ----------
+    curr_dir: str
+              The current directory the user is on.
+
+    back: bool
+          If True, it the returned path will be that of the folder just above the current hierarchy. If it's at the root, it will return the same path.
+    """
+    # path of previous hierarchy
+    if back:
+        if curr_dir.rstrip("/") in ROOT_DIRECTORIES:
+            return ""
+        return os.path.dirname(curr_dir)
+
+    # path of chosen file
     if curr_dir:
-        return curr_dir + "/" + suffix
-    return curr_dir + suffix
+        return os.path.join(curr_dir, suffix)
+    return suffix + "/"
+
+
+def is_hidden(file, path):
+    # todo: fix isse of not finding all hidden files
+    """Checks if the file/folder is hidden or not."""
+    full_file_path = os.path.join(path, file)
+    if file[0] == "$" or file[0] == ".":
+        return True
+
+    return False
+
+
+def rename(curr_dir, old_name, new_name):
+    """Renames the chosen file to the new name."""
+    old_name = create_path(curr_dir, old_name)
+    new_name = create_path(curr_dir, new_name)
+    os.rename(old_name, new_name)
+
+
+ROOT_DIRECTORIES = get_drives()
--- main.py
+++ main.py
@@ -1,32 +1,29 @@
-import os
-import string
 import PySimpleGUI as sg
-from file_handling import open, create_path
+from file_handling import open, create_path, ROOT_DIRECTORIES, rename
 
+# stores the current directory the user is on
 curr_dir = ""
 
-
-def get_drives():
-    """Get all the drives and devices connected on this PC/laptop."""
-    drives = []
-    for letter in string.ascii_uppercase:
-        if os.path.exists(f"{letter}:"):
-            drives.append(f"{letter}:")
-    return drives
-
-
 # ---------- GUI UPDATE FUNCTIONS -----------
-def update_options(choices):
+
+
+def update_options(new_files):
     """Updates the Listbox showing all the folders/files with the given ones."""
+    
     lb = window.find_element("options")
-    lb.update(choices)
+    lb.update(new_files)
+
+
+def refresh():
+    """Refreshes the Listbox by refreshing the files in the current directory."""
+
+    new_files = open(curr_dir)
+    update_options(new_files)
+
 
 # ---------- WINDOW LAYOUT ----------
-
-
-drives = get_drives()
-drives_list = sg.Listbox(
-    values=drives,
+files = sg.Listbox(
+    values=ROOT_DIRECTORIES,
     select_mode="LISTBOX_SELECT_MODE_SINGLE",
     size=(50, 50),
     no_scrollbar=True,
@@ -34,7 +31,15 @@ drives_list = sg.Listbox(
     bind_return_key=True
 )
 
-layout = [[drives_list, sg.Button("Open", key='open_btn')]]
+files_col = [[files]]
+btns_col = [
+    [sg.Button("Go Back", key='back_btn')],
+    [sg.Button("Rename", key="rename-btn")]
+]
+
+layout = [
+    [sg.Column(files_col), sg.Column(btns_col)]
+]
 
 # ------- CREATING WINDOW -------
 window = sg.Window(
@@ -52,11 +57,30 @@ while True:
     # open the file or folder when the option is double clicked
     if event == 'options':
         choice = values['options'][0]
-        path = create_path(choice, curr_dir)
-        new_choices = open(path)
-        print(path)
-        if new_choices is not None:
+        path = create_path(curr_dir, choice)
+        new_files = open(path)
+        if new_files is not None:
             curr_dir = path
-            update_options(new_choices)
+            update_options(new_files)
+        print(curr_dir)
+
+    # go back one hierarchy
+    if event == 'back_btn':
+        curr_dir = create_path(curr_dir, back=True)
+        new_files = open(curr_dir)
+        update_options(new_files)
+
+    # rename file/folder
+    if event == 'rename-btn':
+        if values['options']:
+            new_name = sg.popup_get_text(
+                "Enter the new name:",
+                title="Rename",
+            )
+            rename(curr_dir, values['options'][0], new_name)
+            refresh()
+        else:
+            sg.popup_auto_close("No file was chosen!",
+                                title="Error", auto_close_duration=3)
 
 window.close()
