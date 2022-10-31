84da0343f93e34babeb7613296487d452ce68c60
Nyagami <hoangquan05112002@gmail.com>
2022-10-31 23:01:22 +0700

    change layout

--- layout.py
+++ layout.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
-from file_handling import ROOT_DIRECTORIES
+
 from constants import file_types
+from file_handling import ROOT_DIRECTORIES
 
 
 def create_layout():
@@ -8,12 +9,13 @@ def create_layout():
     files = sg.Listbox(
         values=ROOT_DIRECTORIES,
         select_mode="LISTBOX_SELECT_MODE_SINGLE",
-        right_click_menu=['&Right', ['Back', 'Sort', 'Find', 'Zip', 'Unzip', 'New File', 'New Folder', 'Rename', 'Copy', 'Paste', 'Move', 'Delete']],
-        size=(50, 30),
+        right_click_menu=['&Right',['Back', 'Sort', 'Find', 'Zip', 'Unzip', 'New File', 'New Folder', 'Rename', 'Copy', 'Paste','Move', 'Delete']],
+        size=(150, 30),
         # no_scrollbar=True,
         key='options',
         bind_return_key=True,
         expand_y=True
+
     )
 
     files_col = [[files]]
@@ -21,42 +23,36 @@ def create_layout():
 
     return [
         # [sg.Text(key='message')],
-        [sg.Column(files_col), sg.Column(btns_col)],
-        [sg.Button("Keyboard Shortcuts", key="shortcut_btn")]
-
+        create_buttons(),
+        [sg.Column(files_col)],
+        [sg.Button(image_filename='img_icon\\question.png', key="shortcut_btn",button_color='#FFFFFF',mouseover_colors='#d1d1e0',tooltip="Keyboard Shortcuts")]
     ]
 
 
 def create_buttons():
     buttons = {
-        "back_btn": "Go Back",
-        "sort": "Sort",
-        # "sort_by_name_btn": "Sort By Name",
-        # "sort_by_date_btn": "Sort By Date",
-        # "sort_by_type_btn": "Sort By Type",
-        # "sort_by_size_btn": "Sort By Size",
-        "find_btn": "Find",
-        "zip_btn": "Zip",
-        "unzip_btn": "UnZip",
-        "new_file_btn": "Create New File",
-        "new_dir_btn": "Create New Folder",
-        "rename_btn": "Rename",
-        "copy_btn": "Copy",
-        "paste_btn": "Paste",
-        "move_btn": "Move",
-        "delete_btn": "Delete",
-        "cancel_btn": "Cancel",
+        "back_btn": " Go Back ",
+        "new_file_btn": " Create New File ",
+        "new_dir_btn": " Create New Folder ",
+        "rename_btn": " Rename ",
+        "copy_btn": " Copy ",
+        "paste_btn": " Paste ",
+        "move_btn": " Move ",
+        "delete_btn": " Delete ",
+        "cancel_btn": " Cancel ",
     }
     btns_col = []
-    i = 1
-    no_of_btns = len(buttons)
-    for key, value in buttons.items():
-        btns_col.append([sg.Button(value, key=key, expand_x=True)])
-        if i < no_of_btns:
-            btns_col.append([sg.HorizontalSeparator()])
-        i += 1
-    return btns_col
+    btns_col.append(sg.Button(image_filename='img_icon\\back.png', key="back_btn", expand_x=True, tooltip=" Go Back ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
+    btns_col.append(sg.Button(image_filename='img_icon\\add-file.png', key="new_file_btn", expand_x=True, tooltip=" Create New File ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
+    btns_col.append(sg.Button(image_filename='img_icon\\add-folder.png', key="new_dir_btn", expand_x=True, tooltip=" Create New Folder ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
+    btns_col.append(sg.Button(image_filename='img_icon\\rename.png', key="rename_btn", expand_x=True, tooltip=" Rename ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
+    btns_col.append(sg.Button(image_filename='img_icon\\copy.png', key="copy_btn", expand_x=True, tooltip=" Copy ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
+    btns_col.append(sg.Button(image_filename='img_icon\\paste.png', key="paste_btn", expand_x=True, tooltip=" Paste ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
+    btns_col.append(sg.Button(image_filename='img_icon\\move.png', key="move_btn", expand_x=True, tooltip=" Move ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
+    btns_col.append(sg.Button(image_filename='img_icon\\delete.png', key="delete_btn", expand_x=True, tooltip=" Delete ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
+    btns_col.append(sg.Button(image_filename='img_icon\\cancel.png', key="cancel_btn", expand_x=True, tooltip=" Cancel ",button_color='#FFFFFF',mouseover_colors='#d1d1e0'))
 
+    return btns_col
 
 def file_details_win_layout():
 
